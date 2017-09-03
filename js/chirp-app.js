2e5ead07a3f9a387253cdba73a5d7d877a7fb7df
chainlighting <chainlighting@163.com>
2017-09-03 13:14:48 +0800

    tone frequence test

--- js/chirp-app.js
+++ js/chirp-app.js
@@ -1,7 +1,7 @@
 var ALPHABET = ' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890~`!@#%&*()_-+={}[]:;\"\'|\\<>,.?/';
 var freqCodec = {
-  freqMin: 15000,
-  freqMax: 18000,
+  freqMin: 1200,
+  freqMax: 3000,
   frameStartFreq: 3100,
   frameEndFreq: 3120,
   frameDataSeqFreqs: [3200,3220,3240,3260,3280,3300,3320,3340],
